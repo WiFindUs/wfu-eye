1b7988eb7e9be8c88292a134e6787e62b933b388
Mark Gillard <marzer_@hotmail.com>
2014-09-14 03:03:08 +0930

    fleshing out structure

---
+++ src/com/wifindus/eye/shared/Device.java
@@ -0,0 +1,9 @@
+package com.wifindus.eye.shared;
+
+import java.io.Serializable;
+
+public class Device implements Serializable
+{
+	private long id = 0;
+	private String hash;
+}
---
+++ src/com/wifindus/eye/shared/Incident.java
@@ -0,0 +1,17 @@
+package com.wifindus.eye.shared;
+import java.io.Serializable;
+
+public class Incident implements Serializable
+{
+	public static final byte TYPE_NONE = 0;
+	public static final byte TYPE_SECURITY = 1;
+	public static final byte TYPE_MEDICAL = 2;
+	public static final byte TYPE_ALL = TYPE_SECURITY | TYPE_MEDICAL;
+	
+	private static final long serialVersionUID = 6536603279066529731L;
+	
+	private long id = 0;
+	private byte type;
+	private Location location;
+	
+}
