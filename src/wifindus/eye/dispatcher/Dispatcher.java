92a232cdb3cf3f643310cff22e2032403589530d
Mark Gillard <marzer_@hotmail.com>
2014-09-20 22:45:28 +0930

    added main()

--- src/wifindus/eye/dispatcher/Dispatcher.java
+++ src/wifindus/eye/dispatcher/Dispatcher.java
@@ -1,11 +1,21 @@
 package wifindus.eye.dispatcher;
 
-import wifindus.eye.EyeObject;
+import wifindus.eye.EyeApplication;
 
-public class Dispatcher extends EyeObject
+public class Dispatcher extends EyeApplication
 {
 	public Dispatcher(String[] args)
 	{
 		super(args);
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args)
+	{
+		Dispatcher dispatcher = new Dispatcher(args);
+		dispatcher.dispose();
 	}
 }
