c0d44ea2112cafe709dc684a85505fe8eb70ba78
Mark Gillard <marzer_@hotmail.com>
2014-09-23 05:48:19 +0930

    updated to match EventObject pattern

--- src/wifindus/eye/Device.java
+++ src/wifindus/eye/Device.java
@@ -1,10 +1,133 @@
 package wifindus.eye;
 
-import java.io.Serializable;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Date;
+import wifindus.EventObject;
 
-public class Device implements Serializable
+/**
+ * A client device in use by medical, security or wifindus personnel.
+ * @author Mark 'marzer' Gillard
+ */
+public class Device extends EventObject<DeviceEventListener>
 {
-	private static final long serialVersionUID = -3878642294643791602L;
-	private long id = 0;
-	private String hash;
+	/**
+	 * A description of a client device's 'type'.
+	 * @author Mark 'marzer' Gillard
+	 */
+	public enum Type
+	{
+		/**
+		 * This device is an Android or iOS phone. Since a grey area exists where
+		 * tablets may have cellular connections and make VOIP calls etc,
+		 * a device being of type 'Phone' must be able to do all of the above,
+		 * in addition to being able to make cellular-network-based phone calls.
+		 */
+		Phone,
+		
+		/**
+		 * Any Android or iOS device that does not make cellular phone calls and
+		 * is not a smart watch. 
+		 */
+		Tablet,
+		
+		/**
+		 * Android or iOS smartwatches (galaxy gear, iWatch, etc.)
+		 */
+		Watch,
+		
+		/**
+		 * A laptop/netbook/computer running a wifindus EMT client application.
+		 * Since no such application currently exists (and is unlikely), this will
+		 * probably not be used (placeholder). 
+		 */
+		Computer,
+		
+		/**
+		 * A placeholder for other/unknown device types.
+		 */
+		Other
+	}
+	
+	//properties
+	private String hash = "";
+	private Type type = Type.Other;
+	private InetAddress address = null;
+	private Location location = Location.EMPTY;
+	private Atmosphere atmosphere = Atmosphere.EMPTY;
+	private Date lastUpdate = new Date(0);
+	//database relationships
+	private User currentUser = null;
+	private Incident currentIncident = null;
+	
+	/////////////////////////////////////////////////////////////////////
+	// CONSTRUCTORS
+	/////////////////////////////////////////////////////////////////////
+	
+	public Device(String hash, Type type, DeviceEventListener... listeners)
+	{
+		super(listeners);
+		
+		if (hash == null)
+			throw new IllegalArgumentException("Parameter 'hash' cannot be null.");
+		if (!Hash.isValid(hash))
+			throw new IllegalArgumentException("Parameter 'hash' is not a valid WFU device hash ("+hash+").");
+		if (type == Type.Computer)
+			throw new IllegalArgumentException("Parameter 'type' does not currently support Type.Computer.");
+		
+		this.hash = hash;
+		this.type = type;
+	}
+
+	/////////////////////////////////////////////////////////////////////
+	// PUBLIC METHODS
+	/////////////////////////////////////////////////////////////////////
+	
+	/////////////////////////////////////////////////////////////////////
+	// PROTECTED METHODS
+	/////////////////////////////////////////////////////////////////////
+	
+	@Override
+	protected void mapEvents(String event, DeviceEventListener listener, Object data)
+	{
+		switch(event)
+		{
+			case "created":
+				listener.deviceCreated(this);
+				break;
+			case "timedout":
+				listener.deviceTimedOut(this);
+				break;
+			case "loggedin":
+				listener.deviceUserLoggedIn(this, (User)data);
+				break;
+			case "loggedout":
+				listener.deviceUserLoggedOut(this, (User)data);
+				break;
+			case "location":
+				listener.deviceLocationChanged(this);
+				break;
+			case "atmosphere":
+				listener.deviceAtmosphereChanged(this);
+				break;
+			case "address":
+				listener.deviceAddressChanged(this);
+				break;
+			case "updated":
+				listener.deviceUpdated(this);
+				break;
+			case "assigned":
+				listener.deviceAssignedIncident(this, (Incident)data);
+				break;
+			case "unassigned":
+				listener.deviceUnassignedIncident(this, (Incident)data);
+				break;
+			
+		}
+		
+	}
+	
+	/////////////////////////////////////////////////////////////////////
+	// PRIVATE METHODS
+	/////////////////////////////////////////////////////////////////////
 }
--- src/wifindus/eye/Incident.java
+++ src/wifindus/eye/Incident.java
@@ -1,16 +1,90 @@
 package wifindus.eye;
-import java.io.Serializable;
 
-public class Incident implements Serializable
+import java.util.concurrent.ConcurrentHashMap;
+import wifindus.EventObject;
+
+public class Incident extends EventObject<IncidentEventListener>
 {
-	public static final byte TYPE_NONE = 0;
-	public static final byte TYPE_SECURITY = 1;
-	public static final byte TYPE_MEDICAL = 2;
-	public static final byte TYPE_ALL = TYPE_SECURITY | TYPE_MEDICAL;
 	
-	private static final long serialVersionUID = 6536603279066529731L;
 	
-	private long id = 0;
-	private byte type;
-	private Location location;
+	/**
+	 * A description of an Incident's 'type' (i.e. who is supposed to respond to it).
+	 * @author Mark 'marzer' Gillard
+	 */
+	public enum Type
+	{
+		/**
+		 * This incident is medical in nature, and should be responded to by medical personnel.
+		 */
+		Medical,
+		
+		/**
+		 * This incident is one of security, and should be responded to by security personnel.
+		 */
+		Security,
+		
+		/**
+		 * This incident is technical/internal, and should be responded to by WiFindUs personnel.
+		 */
+		WiFindUs
+	}
+	
+	
+	//properties
+	private int id;
+	private Type type;
+	private Location location = Location.EMPTY;
+	//database relationships
+	private ConcurrentHashMap<String,Device> respondingDevices = new ConcurrentHashMap<>();
+	
+	/////////////////////////////////////////////////////////////////////
+	// CONSTRUCTORS
+	/////////////////////////////////////////////////////////////////////
+	
+	public Incident(int id, Type type, Location location, IncidentEventListener... listeners)
+	{
+		super(listeners);
+		
+		if (id < 0)
+			throw new IllegalArgumentException("Parameter 'id' cannot be negative.");
+		if (location == null)
+			throw new IllegalArgumentException("Parameter 'location' cannot be null.");
+		if (location.isEmpty())
+			throw new IllegalArgumentException("Parameter 'location' cannot be empty.");
+		
+		this.id = id;
+		this.type = type;
+		this.location = location;
+	}
+	
+	/////////////////////////////////////////////////////////////////////
+	// PUBLIC METHODS
+	/////////////////////////////////////////////////////////////////////
+	
+	/////////////////////////////////////////////////////////////////////
+	// PROTECTED METHODS
+	/////////////////////////////////////////////////////////////////////
+	
+	@Override
+	protected void mapEvents(String event, IncidentEventListener listener,
+			Object data)
+	{
+		switch(event)
+		{
+			case "created":
+				listener.incidentCreated(this);
+				break;
+			case "deleted":
+				listener.incidentDeleted(this);
+				break;
+			case "archived":
+				listener.incidentArchived(this);
+				break;	
+		}
+		
+	}
+	
+	/////////////////////////////////////////////////////////////////////
+	// PRIVATE METHODS
+	/////////////////////////////////////////////////////////////////////
 }
