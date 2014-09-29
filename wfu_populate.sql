fbaedc6baf4fd6f025204b00e433c293e06a1045
Mark Gillard <marzer_@hotmail.com>
2014-09-30 07:20:43 +0930

    more detailed test data

--- wfu_populate.sql
+++ wfu_populate.sql
@@ -6,24 +6,24 @@ CREATE DATABASE IF NOT EXISTS wfu_eye_db;
 USE wfu_eye_db;
 
 /*=========================================================== 
-  Devices
+  Device	
   -----------------------------------------------------------*/
 INSERT INTO
-	Devices (hash, deviceType, lastUpdate)
+	Devices (hash, deviceType, lastUpdate, latitude, longitude, accuracy)
 VALUES 
-	('TYRZz3u0','PHO', NOW()),
-	('AbUNZ0xl','PHO', NOW()),
-	('WuQkhzZB','PHO', NOW()),
-	('NsiBN8kt','PHO', NOW()),
-	('xkkk7tzT','PHO', NOW()),
-	('DH1527J9','PHO', NOW()),
-	('RLvlP1Sp','TAB', NOW()),
-	('nopGICOT','TAB', NOW()),
-	('Bj4952Gg','PHO', NOW()),
-	('8SL4oA6X','WAT', NOW());
+	('TYRZz3u0','PHO', NOW(), -34.911282, 138.578099, 20.0),
+	('AbUNZ0xl','PHO', NOW(), -34.912356, 138.580546, 30.0),
+	('WuQkhzZB','PHO', NOW(), -34.918109, 138.582713, 40.0),
+	('NsiBN8kt','PHO', NOW(), -34.915928, 138.584043, 100.0),
+	('xkkk7tzT','PHO', NOW(), -34.919306, 138.580524, 10.0),
+	('DH1527J9','PHO', NOW(), -34.914696, 138.583099, 50.0),
+	('RLvlP1Sp','TAB', NOW(), -34.911142, 138.580803, 100.0),
+	('nopGICOT','TAB', NOW(), -34.911282, 138.578099, 200.0),
+	('Bj4952Gg','PHO', NOW(), -34.918109, 138.582348, 30.0),
+	('8SL4oA6X','WAT', NOW(), -34.915716, 138.581318, 20.0);
 
 /*=========================================================== 
-  Users
+  User	
   -----------------------------------------------------------*/
 INSERT INTO
 	Users (id, nameFirst, nameMiddle, nameLast, personnelType)
@@ -51,15 +51,7 @@ VALUES
 	(17,'Sec', '', 'Test 5', 'SEC');
 	
 /*=========================================================== 
-  DeviceUsers	
-  -----------------------------------------------------------*/
-INSERT INTO
-	DeviceUsers (userID, deviceHash)
-VALUES 
-	(1, 'TYRZz3u0');
-
-/*=========================================================== 
-  Nodes
+  Node	
   -----------------------------------------------------------*/
 INSERT INTO
 	Nodes (hash, latitude, longitude, lastUpdate)
@@ -70,12 +62,40 @@ VALUES
 	('ScJQCjOm', -34.918008, 138.583196, NOW()),
 	('km8rsPfh', -34.917815, 138.579226, NOW()),
 	('hMFoVTyg', -34.916126, 138.581028, NOW());
-	
+    
 /*=========================================================== 
-  Incidents
+  DeviceUsers	
+  -----------------------------------------------------------*/
+INSERT INTO
+	DeviceUsers (userID, deviceHash)
+VALUES 
+	(1, 'TYRZz3u0'),
+	(2, 'AbUNZ0xl'),
+	(3, 'WuQkhzZB'),
+	(8, 'NsiBN8kt'),
+	(9, 'xkkk7tzT'),
+	(13, 'nopGICOT'),
+	(14, '8SL4oA6X');
+  
+/*=========================================================== 
+  Incidents	
   -----------------------------------------------------------*/
 INSERT INTO
 	Incidents (id, incidentType, latitude, longitude, created)
 VALUES 
-	(1, 'SEC', -34.914850, 138.578958, NOW()),
-	(2, 'MED', -34.917093, 138.580020, NOW());
+	(1, 'MED', -34.911282, 138.578099, NOW()),
+	(2, 'SEC', -34.919306, 138.580524, NOW());
+	
+/*=========================================================== 
+  Assign an incidents
+  -----------------------------------------------------------*/
+UPDATE
+	Devices
+SET
+	respondingIncidentID=1
+WHERE
+	hash='xkkk7tzT';
+
+
+
+
