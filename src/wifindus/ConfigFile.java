4efa98ec1440701b1d26ca57995b6f77cb005f52
Mark Gillard <marzer_@hotmail.com>
2014-09-30 07:16:54 +0930

    serializable, transience

--- src/wifindus/ConfigFile.java
+++ src/wifindus/ConfigFile.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
@@ -25,11 +26,12 @@ import java.util.regex.Pattern;
  * Config files may include C++-style comments (//) that will be ignored by the parser.
  * @author Mark 'marzer' Gillard
  */
-public class ConfigFile
+public class ConfigFile implements Serializable
 {
-	private static final Pattern PATTERN_KEY = Pattern.compile( "[a-zA-Z0-9_\\-.]+(?:\\[[ \t]*\\])?" );
-	private static final Pattern PATTERN_COMMENT = Pattern.compile( "//.*$" );
-	private static final Pattern PATTERN_CONFIG_KVP
+	private static final long serialVersionUID = -1878576970458542468L;
+	private transient static final Pattern PATTERN_KEY = Pattern.compile( "[a-zA-Z0-9_\\-.]+(?:\\[[ \t]*\\])?" );
+	private transient static final Pattern PATTERN_COMMENT = Pattern.compile( "//.*$" );
+	private transient static final Pattern PATTERN_CONFIG_KVP
 		= Pattern.compile( "^("+ PATTERN_KEY.pattern() +")[ \t]*[:=][ \t]*(.+)$");
 	
 	private Map<String, String> kvps = new ConcurrentHashMap<>();
