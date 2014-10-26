52ffd429781bd5b34a07ac5332ab5d3ab7eeafbc
Mark Gillard <marzer_@hotmail.com>
2014-10-26 12:15:05 +1030

    renamed members and exposed corner locations

--- src/wifindus/GPSRectangle.java
+++ src/wifindus/GPSRectangle.java
@@ -2,6 +2,8 @@ package wifindus;
 
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.io.Serializable;
 import wifindus.eye.Location;
 
@@ -13,38 +15,35 @@ import wifindus.eye.Location;
 public class GPSRectangle implements Serializable
 {
 	private static final long serialVersionUID = 5246886591603837227L;
-	private double latitudeStart, latitudeEnd, longitudeStart, longitudeEnd;
 	private double width, height;
+	private Location northWest;
+	private Location southEast;
 	private Location center;
 	
 	/**
 	 * Creates a new GPS rectangle using the given coordinates.
-	 * @param latStart The latitude component of the top-left corner of the rectangle.
-	 * @param longStart The longitude component of the top-left corner of the rectangle.
-	 * @param latEnd The latitude component of the bottom-right corner of the rectangle.
-	 * @param longEnd The longitude component of the bottom-right corner of the rectangle.
+	 * @param northLatitude The latitude component of the top-left corner of the rectangle.
+	 * @param westLongitude The longitude component of the top-left corner of the rectangle.
+	 * @param southLatitude The latitude component of the bottom-right corner of the rectangle.
+	 * @param eastLongitude The longitude component of the bottom-right corner of the rectangle.
 	 * @throws IllegalArgumentException if you provide values that don't make sense
 	 */
-	public GPSRectangle(double latStart, double longStart, double latEnd, double longEnd)
+	public GPSRectangle(double northLatitude, double westLongitude, double southLatitude, double eastLongitude)
 	{
-		if (latStart > 90.0 || latStart < -90.0 || latEnd > 90.0 || latEnd < -90.0)
+		if (northLatitude > 90.0 || northLatitude < -90.0 || southLatitude > 90.0 || southLatitude < -90.0)
 			throw new IllegalArgumentException("Latitudes must be between -90.0 and 90.0 (inclusive).");
-		if (longStart > 180.0 || longStart < -180.0 || longEnd > 180.0 || longEnd < -180.0)
+		if (westLongitude > 180.0 || westLongitude < -180.0 || eastLongitude > 180.0 || eastLongitude < -180.0)
 			throw new IllegalArgumentException("Longitudes must be between -180.0 and 180.0 (inclusive).");
-		if (longEnd < longStart)
-			throw new IllegalArgumentException("End longitude must be greater than Start longitude (left-to-right).");
-		if (latStart < latEnd)
+		if (eastLongitude < westLongitude)
+			throw new IllegalArgumentException("East longitude must be greater than West longitude (left-to-right).");
+		if (northLatitude < southLatitude)
 			throw new IllegalArgumentException("End latitude must be lower than Start latitude (top-to-bottom).");
 		
-		latitudeStart = latStart;
-		latitudeEnd = latEnd;
-		longitudeStart = longStart;
-		longitudeEnd = longEnd;
-		
-		width = longitudeEnd - longitudeStart;
-		height = latitudeStart - latitudeEnd;
-		
-		center = new Location(latitudeStart - (height/2.0), longitudeStart + (width/2.0));
+		northWest = new Location(Double.valueOf(northLatitude), Double.valueOf(westLongitude));
+		southEast = new Location(Double.valueOf(southLatitude), Double.valueOf(eastLongitude));
+		width = eastLongitude - westLongitude;
+		height = northLatitude - southLatitude;
+		center = new Location(Double.valueOf(northLatitude - (height/2.0)), Double.valueOf(westLongitude + (width/2.0)));
 	}
 	
 	/**
@@ -55,10 +54,10 @@ public class GPSRectangle implements Serializable
 	 */
 	public boolean contains(double latitude, double longitude)
 	{
-		return latitude <= latitudeStart
-			&& latitude >= latitudeEnd
-			&& longitude >= longitudeStart
-			&& longitude <= longitudeEnd;
+		return latitude <= northWest.getLatitude()
+			&& latitude >= southEast.getLatitude()
+			&& longitude >= northWest.getLongitude()
+			&& longitude <= southEast.getLongitude();
 	}
 	
 	/**
@@ -73,16 +72,39 @@ public class GPSRectangle implements Serializable
 		return contains(coords.getLatitude(), coords.getLongitude());
 	}
 	
+	public Location getNorthWest()
+	{
+		return northWest;
+	}
+	
 	public Location getCenter()
 	{
 		return center;
 	}
 	
+	public Location getSouthEast()
+	{
+		return southEast;
+	}
+	
+	public Point2D.Double translate(Rectangle2D.Double target, double latitude, double longitude)
+	{
+		return new Point2D.Double(
+				target.x + (((longitude - northWest.getLongitude()) / width) * target.width),
+				target.y + (((northWest.getLatitude() - latitude) / height) * target.height)
+			);
+	}
+	
+	public Point2D.Double translate(Rectangle2D.Double target, Location coords)
+	{
+		return translate(target, coords.getLatitude(), coords.getLongitude());
+	}
+	
 	public Point translate(Rectangle target, double latitude, double longitude)
 	{
 		return new Point(
-				target.x + (int)(((longitude - longitudeStart) / width) * (double)target.width),
-				target.y + (int)(((latitudeStart - latitude) / height) * (double)target.height)
+				target.x + (int)(((longitude - northWest.getLongitude()) / width) * (double)target.width),
+				target.y + (int)(((northWest.getLatitude() - latitude) / height) * (double)target.height)
 			);
 	}
 	
