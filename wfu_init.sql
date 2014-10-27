406b41c5164af56a5f7b5a11c6c4dc129541ad42
Mark Gillard <marzer_@hotmail.com>
2014-10-27 16:28:21 +1030

    added stuff for archiving purposes

--- wfu_init.sql
+++ wfu_init.sql
@@ -6,10 +6,11 @@ CREATE DATABASE IF NOT EXISTS wfu_eye_db;
 USE wfu_eye_db;
 
 SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS PastIncidentResponders;
+DROP TABLE IF EXISTS DeviceUsers;
 DROP TABLE IF EXISTS Incidents;
 DROP TABLE IF EXISTS Devices;
 DROP TABLE IF EXISTS Users;
-DROP TABLE IF EXISTS DeviceUsers;
 DROP TABLE IF EXISTS Nodes;
 SET foreign_key_checks = 1;
 
@@ -46,7 +47,16 @@ CREATE TABLE Incidents (
   accuracy     decimal(8, 4) CHECK (accuracy IS NULL OR accuracy BETWEEN 0.0000 AND 9999.9999), 
   created      datetime DEFAULT '1970-01-01 00:00:00' NOT NULL, 
   archived     tinyint DEFAULT 0 NOT NULL, 
+  archivedTime datetime, 
+  severity	   int(10),
+  code		   varchar(32), 
+  reportingUserID int(10), 
   PRIMARY KEY (id)) ENGINE=InnoDB;
+  
+CREATE TABLE PastIncidentResponders (
+  userID     int(10) NOT NULL, 
+  incidentID int(10) NOT NULL, 
+  PRIMARY KEY (userID, incidentID)) ENGINE=InnoDB;
   
 CREATE TABLE DeviceUsers (
   userID     int(10) NOT NULL, 
@@ -67,3 +77,6 @@ CREATE TABLE Nodes (
 ALTER TABLE Devices ADD INDEX `Responded to by` (respondingIncidentID), ADD CONSTRAINT `Responded to by` FOREIGN KEY (respondingIncidentID) REFERENCES Incidents (id);
 ALTER TABLE DeviceUsers ADD INDEX `Uses a Device according to` (userID), ADD CONSTRAINT `Uses a Device according to` FOREIGN KEY (userID) REFERENCES Users (id);
 ALTER TABLE DeviceUsers ADD INDEX `In use by user according to` (deviceHash), ADD CONSTRAINT `In use by user according to` FOREIGN KEY (deviceHash) REFERENCES Devices (hash);
+ALTER TABLE Incidents ADD INDEX `Reported by` (reportingUserID), ADD CONSTRAINT `Reported by` FOREIGN KEY (reportingUserID) REFERENCES Incidents (id);
+ALTER TABLE PastIncidentResponders ADD INDEX `Responded to an incident` (userID), ADD CONSTRAINT `Responded to an incident` FOREIGN KEY (userID) REFERENCES Users (id);
+ALTER TABLE PastIncidentResponders ADD INDEX `Was reponded to by user` (incidentID), ADD CONSTRAINT `Was reponded to by user` FOREIGN KEY (incidentID) REFERENCES Incidents (id);
